53103cb76efe606d12b329a5143b2a1fea121e57
Reed Stewart <reedostewart@gmail.com>
2019-10-21 21:30:59 +1300

    Basic search bar finished, still need to add functionality from context state? Or redux? still unsure

--- client/src/components/layout/Search.js
+++ client/src/components/layout/Search.js
@@ -3,27 +3,27 @@ import React, { useState } from 'react';
 const Search = () => {
   const [formData, setFormData] = useState({
     pid: '',
-    region: '.co.nz'
+    region: ''
   });
 
   return (
     <section id='searchbar'>
       <form>
-        <div className='form-item'>
-          <h2>Product ID:</h2>
-          <input
-            className='input-item'
-            type='text'
-            placeholder='PID'
-            onChange={e => setFormData({ ...formData, pid: e.target.value })}
-          />
-        </div>
+        <input
+          className='input-item'
+          type='text'
+          placeholder='Product ID'
+          onChange={e => setFormData({ ...formData, pid: e.target.value })}
+        />
 
         <select
           name='region'
           className='input-item'
           onChange={e => setFormData({ ...formData, region: e.target.value })}
         >
+          <option value='' disabled selected>
+            Region
+          </option>
           <option value='.co.nz'>NZ</option>
           <option value='.com.au'>AU</option>
         </select>
