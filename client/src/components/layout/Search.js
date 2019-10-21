e1f3defbf6252a914c6997913a5bcfd480c0d785
Reed Stewart <reedostewart@gmail.com>
2019-10-21 21:26:05 +1300

    Search bar styling

--- client/src/components/layout/Search.js
+++ client/src/components/layout/Search.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Search = () => {
+  const [formData, setFormData] = useState({
+    pid: '',
+    region: '.co.nz'
+  });
+
   return (
     <section id='searchbar'>
-      <input type='text' />
-      <input type='text' />
-      <button>Search</button>
+      <form>
+        <div className='form-item'>
+          <h2>Product ID:</h2>
+          <input
+            className='input-item'
+            type='text'
+            placeholder='PID'
+            onChange={e => setFormData({ ...formData, pid: e.target.value })}
+          />
+        </div>
+
+        <select
+          name='region'
+          className='input-item'
+          onChange={e => setFormData({ ...formData, region: e.target.value })}
+        >
+          <option value='.co.nz'>NZ</option>
+          <option value='.com.au'>AU</option>
+        </select>
+        <button className='input-item'>Search</button>
+      </form>
     </section>
   );
 };
