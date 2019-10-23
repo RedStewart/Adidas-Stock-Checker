2a11ab667cbbf2f483d1ba6330008e9631716f0d
Reed Stewart <reedostewart@gmail.com>
2019-10-23 21:26:52 +1300

    Just displaying relevent data to user

--- client/src/components/adidas/ShoeInfo.js
+++ client/src/components/adidas/ShoeInfo.js
@@ -5,10 +5,20 @@ const ShoeInfo = () => {
   const adidasContext = useContext(AdidasContext);
   const { product } = adidasContext;
 
+  const imageArr = product.view_list;
+
   return (
     <>
-      <h2>{product.product_description.title}</h2>
+      <h2>{product.name}</h2>
       <h3>{product.attribute_list.color}</h3>
+      <h3>PID: {product.id}</h3>
+      <h3>Price: ${product.pricing_information.currentPrice}.00</h3>
+      <h4>
+        URL:{' '}
+        <a href={'https:' + product.meta_data.canonical} target='_blank'>
+          {product.meta_data.canonical.substring(2)}
+        </a>
+      </h4>
     </>
   );
 };
